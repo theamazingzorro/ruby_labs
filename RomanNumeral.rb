4c9a169ef490352a4a71457abf03cb5d25a6133f
Fox Noland <fnoland@manifestcorp.com>
2021-11-15 17:30:36 -0500

    cleanup

--- RomanNumeral.rb
+++ RomanNumeral.rb
@@ -2,7 +2,7 @@ def convert_to_roman(number)
 	result = ''
 
 	numerals = ['M', 'CM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I']
-	numbers = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]
+	numbers  = [1000, 900, 500,  400,  100,  90,  50,   40,  10,    9,   5,    4,   1]
 
 	for i in 0..numerals.length-1
 		while number >= numbers[i]
